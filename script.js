c47723d33a2f5057d27019277e104bef44d292d9
Noah Salmon <noahsalmon98@gmail.com>
2023-08-25 16:46:01 -0400

    added current date

--- script.js
+++ script.js
@@ -21,3 +21,6 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
+// Display Current date
+  let curentDate = dayjs().format('MMM D, YYYY, hh:mm a')
+  $('#currentDate').text(currentDate);
