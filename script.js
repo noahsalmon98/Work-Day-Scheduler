1ed0b9ee7637f8a8ad672bea5f1f1e653fd8972a
Noah Salmon <noahsalmon98@gmail.com>
2023-08-25 17:32:03 -0400

    added retrieve info from local storage

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
 // Display Current date
-$(function start(){
+function start(){
     setInterval(function () {
         let currentTime  = dayjs().format('MMM D, YYYY, hh:mm a')
         $('#currentDate').text(currentTime);
     }, 1000);
-  });
+  };
   start ()
 
   //save button adding to local storage
@@ -35,4 +35,23 @@ $(function start(){
     var description = $(this).siblings('.description').val()
     var timeBlock = $(this).parent().attr('id')
     localStorage.setItem(timeBlock, description);
+});
+
+// retrieve items from local storage
+$('#hour-9 .description').val(localStorage.getItem("hour-9"))
+$('#hour-10 .description').val(localStorage.getItem("hour-10"))
+$('#hour-11 .description').val(localStorage.getItem("hour-11"))
+$('#hour-12 .description').val(localStorage.getItem("hour-12"))
+$('#hour-13 .description').val(localStorage.getItem("hour-13"))
+$('#hour-14 .description').val(localStorage.getItem("hour-14"))
+$('#hour-15 .description').val(localStorage.getItem("hour-15"))
+$('#hour-16 .description').val(localStorage.getItem("hour-16"))
+$('#hour-17 .description').val(localStorage.getItem("hour-17"))
+$('#hour-18 .description').val(localStorage.getItem("hour-18"))
+$('#hour-19 .description').val(localStorage.getItem("hour-19"))
+$('#hour-20 .description').val(localStorage.getItem("hour-20"))
+
+
+
+
 });
